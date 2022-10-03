22cf728bee49e702b68fe5beba15437421f08445
AlexandruSergheiManea <114501512+AlexandruSergheiManea@users.noreply.github.com>
2022-10-03 09:44:03 +0100

    Add files via upload

---
+++ chat bot.py
@@ -0,0 +1,17 @@
+
+name = input("BOT: What is your name?")
+day = input ( "BOT:" + name + " that is a cool name how are you feeling?")
+positive = ["happy", "great", "good", "alright", "fine", "all good", "not bad", "amazing"]
+bad = ["sad", "bad", "awful", "upsetting", "horrid", "not good", "distrought",]
+if day in positive:
+    print( "BOT: Thats good!")
+elif day in bad:
+    print ( "BOT: Thats not good :( ")
+    
+movie = input ("BOT: What is your fav movie?")
+print ("BOT: I heard" + movie + " was good I've never seen it tho")
+
+
+    
+
+
